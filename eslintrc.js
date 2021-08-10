b40a0927f8d0a9c5ae624553547f14b63c25d9cf
zhangyuang <leonin0829@gmail.com>
2021-08-10 17:30:52 +0800

    chore: off import/export

--- eslintrc.js
+++ eslintrc.js
@@ -34,6 +34,7 @@ module.exports = {
     '@typescript-eslint/strict-boolean-expressions': 'off',
     '@typescript-eslint/consistent-type-assertions': 'off',
     '@typescript-eslint/restrict-plus-operands': 'off',
-    '@typescript-eslint/restrict-template-expressions': 'off'
+    '@typescript-eslint/restrict-template-expressions': 'off',
+    'import/export': 'off'
   }
 }
